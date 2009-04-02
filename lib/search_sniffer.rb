14868f6f3fc5830bdaf6e0ab5eb132497a3d80cd
Tim Underwood <tim@tp-macbook.local>
2009-04-02 16:37:05 -0700

    Refactor the code to simplify it, increase performance, and fix 2 bugs
    - Simplified the code so it's easier to read
    - Return sooner if the referer is blank or the query string is blank which should slightly increase performance
    - SearchReferers and StopWords don't need to be created in the initialize method (performance increase?)
    
    The refactoring solves 2 bugs present in the original code
    
    1. If the referer query string has spaces in it URI.split fails
    2. When query_args was nil (from URI.split failing) the "unless query_args.empty?" line was failing and throwing an exception causing Rails to throw a 500 error
    
    #1 is now properly handled by not using URI.split and #2 doesn't exist with the refactored code.

--- lib/search_sniffer.rb
+++ lib/search_sniffer.rb
@@ -24,60 +24,43 @@ module Squeejee  #:nodoc:
       
       class ReferringSearch
         
+        SearchReferers = {
+              :google     => [/^http:\/\/(www\.)?google.*/, 'q'],
+              :yahoo      => [/^http:\/\/search\.yahoo.*/, 'p'],
+              :msn        => [/^http:\/\/search\.msn.*/, 'q'],
+              :live        => [/^http:\/\/search\.live.*/, 'q'],
+              :aol        => [/^http:\/\/search\.aol.*/, 'userQuery'],
+              :altavista  => [/^http:\/\/(www\.)?altavista.*/, 'q'],
+              :feedster   => [/^http:\/\/(www\.)?feedster.*/, 'q'],
+              :lycos      => [/^http:\/\/search\.lycos.*/, 'query'],
+              :alltheweb  => [/^http:\/\/(www\.)?alltheweb.*/, 'q']
+            }
+            
+        StopWords = /\b(\d+|\w|about|after|also|an|and|are|as|at|be|because|before|between|but|by|can|com|de|do|en|for|from|has|how|however|htm|html|if|i|in|into|is|it|la|no|of|on|or|other|out|since|site|such|than|that|the|there|these|this|those|to|under|upon|vs|was|what|when|where|whether|which|who|will|with|within|without|www|you|your)\b/i
+        
         attr_reader :search_terms # sanitized search terms
         attr_reader :raw # original terms as typed by user
         attr_reader :engine # search engine
       
         def initialize(referer)
+          return if referer.blank?
           
-          @search_referers = {
-                :google     => [/^http:\/\/(www\.)?google.*/, 'q'],
-                :yahoo      => [/^http:\/\/search\.yahoo.*/, 'p'],
-                :msn        => [/^http:\/\/search\.msn.*/, 'q'],
-                :aol        => [/^http:\/\/search\.aol.*/, 'userQuery'],
-                :altavista  => [/^http:\/\/(www\.)?altavista.*/, 'q'],
-                :feedster   => [/^http:\/\/(www\.)?feedster.*/, 'q'],
-                :lycos      => [/^http:\/\/search\.lycos.*/, 'query'],
-                :alltheweb  => [/^http:\/\/(www\.)?alltheweb.*/, 'q'] 
-              }
-              
-          @stop_words = /\b(\d+|\w|about|after|also|an|and|are|as|at|be|because|before|between|but|by|can|com|de|do|en|for|from|has|how|however|htm|html|if|i|in|into|is|it|la|no|of|on|or|other|out|since|site|such|than|that|the|there|these|this|those|to|under|upon|vs|was|what|when|where|whether|which|who|will|with|within|without|www|you|your)\b/i
+          query_string = referer.split('?',2)[1]
+          return if query_string.blank?
           
-          # Get query args
-          query_args =
-            begin
-              URI.split(referer)[7]
-            rescue URI::InvalidURIError
-              nil
-            end
+          SearchReferers.each do |engine,v|
+            pattern,query_param_name = v
+            next unless pattern.match(referer)
             
-          # Determine the referring search that was used
-          unless referer.blank?
-            @search_referers.each do |k, v|
-              reg, query_param_name = v
-              # Check if the referrer is a search engine we are targetting
-              if (reg.match(referer))
-
-                # set the search engine
-                @engine = k 
-
-                unless query_args.empty?
-                  query_args.split("&").each do |arg|
-                    pieces = arg.split('=')
-                    if pieces.length == 2 && pieces.first == query_param_name
-                      unstopped_keywords = CGI.unescape(pieces.last)
-                      @raw = unstopped_keywords
-                      @search_terms = unstopped_keywords.gsub(@stop_words, '').squeeze(' ')
-                      #logger.info("Referring Search Keywords: #{search_terms}")
-                      return true
-                    end
-                  end
-                end # unless
-                
-                return true # because we found a match
-              end # if
-            end # do
-          end #unless          
+            @engine = engine
+            
+            params = CGI::parse(query_string)
+            break unless params.has_key?(query_param_name)
+            
+            @raw = params[query_param_name].join(' ')
+            @search_terms = @raw.gsub(StopWords,'').squeeze(' ')
+            break
+          end
         end #initialize
         
         # Return the referring search string instead of the object serialized into a string
