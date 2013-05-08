a4809b415d0ad6542ff3e71cb82840336e210098
glaszig <glaszig@gmail.com>
2013-05-08 17:30:55 +0200

    simplifying regular expressions

--- lib/sofatutor/search_sniffer/referring_search.rb
+++ lib/sofatutor/search_sniffer/referring_search.rb
@@ -3,22 +3,24 @@ module Sofatutor  #:nodoc:
 
     class ReferringSearch
 
+      # Search engine detetion expressions
       SearchReferers = {
-        :google     => [/^http:\/\/(www\.)?google.*/, 'q'],
-        :yandex     => [/^http:\/\/(www\.)?yandex.*/, 'text'],
-        :mail       => [/^http:\/\/go\.mail.*/, 'q'],
-        :nigma      => [/^http:\/\/(www\.)?nigma.*/, 's'],
-        :bing       => [/^http:\/\/(www\.)?bing.*/, 'q'],
-        :ask        => [/^http:\/\/(www\.)?ask.*/, 'q'],
-        :yahoo      => [/^http:\/\/search\.yahoo.*/, 'p'],
-        :msn        => [/^http:\/\/search\.msn.*/, 'q'],
-        :aol        => [/^http:\/\/search\.aol.*/, 'userQuery'],
-        :altavista  => [/^http:\/\/(www\.)?altavista.*/, 'q'],
-        :feedster   => [/^http:\/\/(www\.)?feedster.*/, 'q'],
-        :lycos      => [/^http:\/\/search\.lycos.*/, 'query'],
-        :alltheweb  => [/^http:\/\/(www\.)?alltheweb.*/, 'q']
+        :google     => [%r{^http://(www\.)?google.*}, 'q'],
+        :yandex     => [%r{^http://(www\.)?yandex.*}, 'text'],
+        :mail       => [%r{^http://go\.mail.*}, 'q'],
+        :nigma      => [%r{^http://(www\.)?nigma.*}, 's'],
+        :bing       => [%r{^http://(www\.)?bing.*}, 'q'],
+        :ask        => [%r{^http://(www\.)?ask.*}, 'q'],
+        :yahoo      => [%r{^http://search\.yahoo.*}, 'p'],
+        :msn        => [%r{^http://search\.msn.*}, 'q'],
+        :aol        => [%r{^http://search\.aol.*}, 'userQuery'],
+        :altavista  => [%r{^http://(www\.)?altavista.*}, 'q'],
+        :feedster   => [%r{^http://(www\.)?feedster.*}, 'q'],
+        :lycos      => [%r{^http://search\.lycos.*}, 'query'],
+        :alltheweb  => [%r{^http://(www\.)?alltheweb.*}, 'q']
       }
 
+      # Words to exclude when compiling #search_terms
       StopWords = /\b(\d+|\w|about|after|also|an|and|are|as|at|be|because|before|between|but|by|can|com|de|do|en|for|from|has|how|however|htm|html|if|i|in|into|is|it|la|no|of|on|or|other|out|since|site|such|than|that|the|there|these|this|those|to|under|upon|vs|was|what|when|where|whether|which|who|will|with|within|without|www|you|your)\b/i
 
       attr_reader :search_terms # sanitized search terms
